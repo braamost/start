62185d4766de5984bd620e1c856dd3b91adf68bc
braamost <150168477+braamost@users.noreply.github.com>
2024-10-20 17:08:22 +0300

    Update guess.js

--- guess.js
+++ guess.js
@@ -6,7 +6,13 @@ let nOfGuesses = 1;
 function check_guess(){
     guess = document.getElementById("guess").value;
     guess = Number(guess);
-    if(guess == x){
+    if(isNaN(guess)){
+        document.getElementById("screen").innerHTML = "please enter a number!";
+    }
+    else if(guess < 1 || guess > 100){
+        document.getElementById("screen").innerHTML = "the number should be between 1 and 100!";
+    }
+    else if(guess == x){
         alert(`correct!\nyou won after ${nOfGuesses} guesses`);
         window.location.reload();
     }
